62ae994e871edb024ef230f02fdaaa06efede815
paper-clips <mustali.helga@gmail.com>
2023-02-04 22:10:14 -0500

    Added loop so application is continuous

--- dogs.py
+++ dogs.py
@@ -8,56 +8,59 @@ import re
 console = Console()
 
 # Get dog breed from user
-breed = input("Name a dog breed (or type \"random\" or \"list\"): ")
+breed = input("Name a dog breed (or type \"random\" or \"list\" or \"end\"): ")
 
-# Random dog picture
-if breed == "random":
-    # API GET request
-    response = requests.get("https://dog.ceo/api/breeds/image/random")
-    #print(json.dumps(response.json(), indent=4))       # Print request info to debug
+while breed != "end":
+    # Random dog picture
+    if breed == "random":
+        # API GET request
+        response = requests.get("https://dog.ceo/api/breeds/image/random")
+        #print(json.dumps(response.json(), indent=4))       # Print request info to debug
 
-    # Verify API request, exit app if error
-    if response.status_code != 200:
-        # API request not successful
-        console.print("Unsuccessful API request.", style="bold red")
-        exit()
+        # Verify API request, exit app if error
+        if response.status_code != 200:
+            # API request not successful
+            console.print("Unsuccessful API request.", style="bold red")
+            exit()
 
-    # Retrieve and save image
-    web_image = requests.get(response.json()["message"]).content
-    with open('dogImage.jpg', 'wb') as img:
-        img.write(web_image)
+        # Retrieve and save image
+        web_image = requests.get(response.json()["message"]).content
+        with open('dogImage.jpg', 'wb') as img:
+            img.write(web_image)
 
-    console.print("Image found!", style="bold green")
-# List breed types
-elif breed == "list":
-    print("Breeds list:")
-    with open('dogBreedsCSV.csv', mode='r') as dogBreedsCSV:
-        dogBreeds = csv.reader(dogBreedsCSV)
-        for dogBreed in dogBreeds:
-            pprint(dogBreed)
-# Specific dog breed picture
-else:
-    # API GET request
-    response = requests.get("https://dog.ceo/api/breed/" + breed + "/images/random")
-    #print(json.dumps(response.json(), indent=4))       # Print request info to debug
+        console.print("Image found!", style="bold green")
+    # List breed types
+    elif breed == "list":
+        print("Breeds list:")
+        with open('dogBreedsCSV.csv', mode='r') as dogBreedsCSV:
+            dogBreeds = csv.reader(dogBreedsCSV)
+            for dogBreed in dogBreeds:
+                pprint(dogBreed)
+    # Specific dog breed picture
+    else:
+        # API GET request
+        response = requests.get("https://dog.ceo/api/breed/" + breed + "/images/random")
+        #print(json.dumps(response.json(), indent=4))       # Print request info to debug
 
-    # Verify input and API request, exit app if errors
-    if response.status_code != 200:
-        # API request not successful
-        console.print("Unsuccessful API request.", style="bold red")
-        exit()
-    if 'message' not in response.json():
-        # Invalid breed
-        console.print("Invalid breed type.", style="bold red")
-        exit()
-    if not response.json()["message"].startswith("https://images.dog.ceo/breeds/"):
-        # Invalid URL
-        console.print("Invalid return address. (On the API's end)", style="bold red")
-        exit()
+        # Verify input and API request, exit app if errors
+        if response.status_code != 200:
+            # API request not successful
+            console.print("Unsuccessful API request.", style="bold red")
+            exit()
+        if 'message' not in response.json():
+            # Invalid breed
+            console.print("Invalid breed type.", style="bold red")
+            exit()
+        if not response.json()["message"].startswith("https://images.dog.ceo/breeds/"):
+            # Invalid URL
+            console.print("Invalid return address. (On the API's end)", style="bold red")
+            exit()
 
-    # Retrieve and save image
-    web_image = requests.get(response.json()["message"]).content
-    with open('dogImage.jpg', 'wb') as img:
-        img.write(web_image)
+        # Retrieve and save image
+        web_image = requests.get(response.json()["message"]).content
+        with open('dogImage.jpg', 'wb') as img:
+            img.write(web_image)
 
-    console.print(breed.capitalize() + " image found!", style="bold green")
+        console.print(breed.capitalize() + " image found!", style="bold green")
+
+    breed = input("Name a dog breed (or type \"random\" or \"list\" or \"end\"): ")
